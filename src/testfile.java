ab65e0d2f7b6059b92f6c91b60a94bd2b8ac6ab9
BT_1N3_17 <jlin7500@bths.edu>
2018-09-27 10:14:59 -0400

    almost done

--- src/testfile.java
+++ src/testfile.java
@@ -1,6 +1,29 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class testfile {
     public static void main(String[] args)
     {
-        
+        //double computerGuess = Math.random();
+        //int computerGuessInt = (int)computerGuess;
+        System.out.println("Choose a number from 0 - 100 and I will try to guess it");
+        Scanner input = new Scanner(System.in);
+        int playerGuess = input.nextInt();
+        Random computerGuess = new Random();
+        int computerGuessInt = computerGuess.nextInt(101);
+        while(computerGuessInt != playerGuess)
+        {
+            String answer = input.nextLine();
+            if(answer.equals("yes"))
+            {
+                System.out.println("it works");
+                return;
+            }
+            computerGuess = new Random();
+            computerGuessInt = computerGuess.nextInt(101);
+            System.out.println("Is" + " " + computerGuessInt + " " + "Your number");
+            System.out.println("If no, type 'higher' if its higher and 'lower' if its lower");
+        }
+        //String higherOrLower = input.nextLine();
     }
 }
