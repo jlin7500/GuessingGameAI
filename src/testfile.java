6c098902e2ac1bca6366e8788b57ef4c72bda2e8
BT_1N3_17 <jlin7500@bths.edu>
2018-09-28 10:17:22 -0400

    Basically done

--- src/testfile.java
+++ src/testfile.java
@@ -9,23 +9,26 @@ public class testfile {
         System.out.println("Choose a number from 0 - 100 and I will try to guess it");
         System.out.println("If the guess is wrong, type 'higher'(case sensitive) if its higher and 'lower'(case sensitive) if its lower");
         Scanner input = new Scanner(System.in);
-        //double computerNumber = Math.random()*100;
-        //int computerGuessInt = (int)computerNumber;
-        //System.out.println(computerGuessInt);
         int computerGuessInt = 100;
+        int computerGuess = computerGuessInt/2;
         int playerNumber =input.nextInt();
+        int range = 25;
         System.out.println("Is 50 your number?");
         while(computerGuessInt != playerNumber)
         {
             String higher = input.nextLine();
-            int computerGuess = computerGuessInt/2;
             if(higher.equals("higher"))
             {
-                int computerGuess2 = computerGuess/2 + 50;
-                System.out.println("Is" + " " + computerGuess2 + " " + "your number?");
+                computerGuess = computerGuess + range;
+                range = range/2+1;
+                System.out.println("Is" + " " + computerGuess + " " + "your number?");
             }
-
-
+            if(higher.equals("lower"))
+            {
+                computerGuess = computerGuess - range;
+                range = range/2 +1;
+                System.out.println("Is" + " " + computerGuess + " " + " your number?");
+            }
         }
         String answer = input.nextLine();
         if(answer.equals("yes"))
